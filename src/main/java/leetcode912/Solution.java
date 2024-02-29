f98a9440133454f0c9712d428b97c9d2f2233e8d
Zhengk <272991962@qq.com>
2024-02-29 13:02:06 +0800

    lt 912 计数排序

--- src/main/java/leetcode912/Solution.java
+++ src/main/java/leetcode912/Solution.java
@@ -1,2 +1,20 @@
-package leetcode912;public class Solution {
+package leetcode912;
+
+// 计数排序
+public class Solution {
+    public int[] sortArray(int[] nums) {
+        int[] count = new int[100000 + 1];
+        for (int num : nums) {
+            count[num + 5 * 10000]++;
+        }
+        int[] res = new int[nums.length];
+        int index = 0;
+        for (int i = 0; i < count.length; i++) {
+            int c = count[i];
+            while (c-- > 0) {
+                res[index++] = i - 5 * 10000;
+            }
+        }
+        return res;
+    }
 }
