fd91abf4ad4d893be604c7119485c860c92ffd48
Zhengk <272991962@qq.com>
2024-03-02 16:08:32 +0800

    lt102 二叉树BFS

---
+++ src/main/java/leetcode102/Solution.java
@@ -0,0 +1,67 @@
+package leetcode102;
+
+import com.sun.source.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        LinkedList<TreeNode> nodes = new LinkedList<>();
+        nodes.add(root);
+        while (!nodes.isEmpty()) {
+            List<Integer> values = new LinkedList<>();
+            int size = nodes.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = nodes.poll();
+                if (node != null) {
+                    values.add(node.val);
+                    nodes.add(node.left);
+                    nodes.add(node.right);
+                }
+            }
+            if (!values.isEmpty()) {
+                list.add(values);
+            }
+
+        }
+        return list;
+    }
+}
