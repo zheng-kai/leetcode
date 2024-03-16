b8376759261b82cc1bbc9f7ac5955a3cfb43f92f
Alex <2729919762@qq.com>
2024-03-16 14:49:24 +0800

    lt94 递归.中序遍历

---
+++ src/main/java/leetcode94/Solution.java
@@ -0,0 +1,57 @@
+package leetcode94;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    List<Integer> l = new LinkedList<Integer>();
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        t(root);
+        return l;
+    }
+
+    public void t(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        t(root.left);
+        l.add(root.val);
+        t(root.right);
+    }
+}
